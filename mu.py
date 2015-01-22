2da51a75380d2e79a4db104f8e81c32678f9d561
efs26 <eric.steinlauf@gmail.com>
2015-01-21 22:38:51 -0500

    add .gitignore and a comment

---
+++ mu.py
@@ -0,0 +1,42 @@
+import re
+
+def r1(s):
+  if s[-1] == 'I':
+    return [s + 'U']
+  else:
+    return []
+
+def r2(s):
+  if s[0] == 'M':
+    return [s + s[1:]]
+  else:
+    return []
+
+def r3(s):
+  return [s[:m.start()] + 'U' + s[m.start() + 3:] for m in
+      re.finditer('(?=III)', s)]
+
+def r4(s):
+  return [s[:m.start()] + s[m.start() + 2:] for m in
+      re.finditer('(?=UU)', s)]
+
+fns = [r1, r2, r3, r4]
+
+def search(axiom, goal):
+  i = 0
+  collection = {axiom: []} 
+  while not collection.has_key(goal):
+    i += 1
+    print i, 'collection size:', len(collection)
+    cprime = {}
+    for s in collection:
+      path = list(collection[s])
+      path.append(s)
+      for fn in fns:
+        sprimes = fn(s)
+        for sprime in sprimes:
+          if sprime == goal:
+            return path
+          if not collection.has_key(sprime):
+            cprime[sprime] = path
+    collection = cprime
